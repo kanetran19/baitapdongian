2e861656d5a4ccb1a34fade9dfcf1cb09728b089
kanetran19 <tranphuocphukhanhvl2014@gmail.com>
2022-08-02 00:00:24 +0700

    co database va cau query

---
+++ database_superstore/People.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS People (
+    `Person` VARCHAR(17) CHARACTER SET utf8,
+    `Region` VARCHAR(7) CHARACTER SET utf8
+);
+INSERT INTO People VALUES
+    ('Anna Andreadi','West'),
+    ('Chuck Magee','East'),
+    ('Kelly Williams','Central'),
+    ('Cassandra Brandow','South');
